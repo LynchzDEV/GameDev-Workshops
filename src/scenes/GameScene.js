80e0acc629e8b92a9860e45bcc3f150b1c2f786d
LynchzDEV <opmok48@gmail.com>
2023-08-09 10:09:59 +0700

    heg yeah

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,23 +1,40 @@
 import Phaser from "phaser";
-
-class GameScene extends Phaser.Scene {
+let player;
+let monster;
+let event;
+let monGroup;
+let cat;
+class EventScene extends Phaser.Scene {
     constructor(test) {
         super({
-            key: "GameScene",
+            key: "EventScene",
         });
     }
-
     preload() {
-        // code here
+        this.load.image("R", "src/image/TimeEventClass/Rapid.png"); // player        this.load.image('S', 'src/image/TimeEventClass/Separate.png') //monster        // this.load.image('cat', 'src/image/playermeow.jpg')
     }
 
     create() {
-        // code here
+        //player        player = this.physics.add.image(225, 600, 'R')
+        player.setImmovable(); //player ไม่ขยับ        //cat        // cat = this.add.image(player.x , player.y, 'cat').setScale(0.25)        //monster
+        monGroup = this.physics.add.group(); //สร้าง group object        //monster spawn
+        event = this.time.addEvent({
+            delay: 3000,
+            callback: function () {
+                monster = this.physics.add.image(225, 150, "S");
+                monGroup.add(monster);
+                monGroup.setVelocityY(200);
+                this.physics.add.collider(player, monster, () => {
+                    monster.destroy();
+                    event.destroy();
+                });
+            },
+            callbackScope: this,
+            loop: true, // repeat: 3 //เริ่มนับจาก ครั้งต่อไป
+        });
     }
-
     update(delta, time) {
-        // code here
+        // for(let i = 0; i < monGroup.getChildren().length; i++){ //ทำให้เรารู้ว่าใน group มีกี่ตัว        //     if(monGroup.getChildren()[i].y > 300){ //i จะเก็บ index ทุกตัว        //         monGroup.getChildren()[i].destroy()        //     }        // }    }
     }
 }
-export default GameScene;
-
+export default EventScene;
